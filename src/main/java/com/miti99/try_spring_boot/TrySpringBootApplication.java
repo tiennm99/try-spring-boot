1151d5dd20c9145a187d2d3a17610f8e5a2aab86
tiennm99 <tiennm99@outlook.com>
2024-10-14 21:13:05 +0700

    [Init]

---
+++ src/main/java/com/miti99/try_spring_boot/TrySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.miti99.try_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miti99/try_spring_boot/TrySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miti99.try_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
